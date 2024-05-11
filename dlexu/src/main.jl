2cbf7c3ce5ee43c90fbaf0a891f161549b2700e4
Mark Nilan <mark7498@outlook.com>
2024-05-11 15:31:53 +0800

    fun with UIs

--- dlexu/src/main.jl
+++ dlexu/src/main.jl
@@ -2,13 +2,14 @@
 include("../../jlib/jlib.jl")
 include("./config.jl")
 include("./structs.jl")
+include("./gui.jl")
 
 function main()
-    jlib.disptm("dlexu began at")
-    println(ARGS[1])
-    dlexuTOML = config.getTOML(ARGS[1])
-    println("dlexuTOML is : $(dlexuTOML)")
-
+    #jlib.disptm("dlexu began at")
+    #println(ARGS[1])
+    #dlexuTOML = config.getTOML(ARGS[1])
+    #println("dlexuTOML is : $(dlexuTOML)")
+    t = gui.DlexUI()
 end
 
 main()

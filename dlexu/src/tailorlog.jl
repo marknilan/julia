6271768df4e07b752a8adf66482222e248ee67bf
Mark Nilan <mark7498@outlook.com>
2024-06-20 09:53:13 +0800

    got the thing working

--- dlexu/src/tailorlog.jl
+++ dlexu/src/tailorlog.jl
@@ -11,44 +11,71 @@ function apply_log_chng(enclprobs, dlexucfg)::Bool
         m[enclprobs[i].encl1] = dlexucfg.delimiter
         m[enclprobs[i].encl2] = dlexucfg.delimiter
     end
-    open(dlexucfg.logfile) do f
+    open(dlexucfg.logfile) do lf
         line = 0
         strcol = ""
-        while !eof(f)
+        while !eof(lf)
             s = string(
                 rstrip(
-                    lstrip(strip(readline(f)), collect(dlexucfg.inquote)),
+                    lstrip(strip(readline(lf)), collect(dlexucfg.inquote)),
                     collect(dlexucfg.inquote),
                 ),
             )
             s = check_qte_delm(s, dlexucfg.quotes)
             s = check_qte_delm(s, dlexucfg.delimiter)
+            println("s WAS $(s)")            
             strarray =
                 split(s, jlib.create_compound_delim(dlexucfg.inquote, dlexucfg.indelm))
+            idx = 1    
             for strcol in strarray       
                 for i in eachindex(enclprobs)
-                    d = findfirst(enclprobs[i].encl1,strcol)
-                    if d != nothing                    
-                        println("f was $(d)")
-                    end    
-                    #before = strcol[1:f[1]-1]
-                    #println("before was $(before)")
-                    #after = strcol[1:findfirst(enclprobs[i].encl1,strcol)[1]-1]
-                    #println("u was $(u)")
-                    
-                    r1 = enclprobs[i].encl1
-                    println("r1 is $(r1)")
-                    r2 = enclprobs[i].encl2
-                    println("r2 is $(r2)")
-                    strcol = replace(strcol, r1 => dlexucfg.delimiter)
-                    strcol = replace(strcol, r2 => dlexucfg.delimiter)
-                    println("strcol is now $(strcol)")
-                end               
-            end
-            
+                    #println("strcol WAS $(strcol)")
+                    st = findfirst(enclprobs[i].encl1,strcol)                    
+                    #if st != nothing                    
+                    #    println("at line $(line) $(enclprobs[i].encl1) st was $(st)")                        
+                    #    before = strcol[1:st[1]-1]
+                    #    println("before was $(before)")
+                    #end    
+                    nd = findlast(enclprobs[i].encl2,strcol)
+                    #if nd != nothing                    
+                    #    println("at line $(line) $(enclprobs[i].encl2) nd was $(nd)")                        
+                    #    after = strcol[nd[1]+1:length(strcol)]
+                    #    println("after was $(after)")
+                    #end   
+                    if st == nothing || nd == nothing
+                        if idx < length(strarray)
+                           strarray[idx] = strarray[idx] * dlexucfg.delimiter
+                        end   
+                        continue
+                    else    
+                       #middle = strcol[st[1]:nd[1]]
+                       strcol = replace(strcol, enclprobs[i].encl1 => dlexucfg.delimiter)
+                       strcol = replace(strcol, enclprobs[i].encl2 => dlexucfg.delimiter)
+                       #println("strcol IS NOW $(strcol)")
+                       #println("middle was $(middle)")
+                    end   
+                    #if length(before) > 0 && length(after) > 0
+                    #    middle = strcol[st[1]:nd[1]]
+                    #    println("middle was $(middle)")
+                    #end                                     
+
+                    #r1 = enclprobs[i].encl1
+                    #println("r1 is $(r1)")
+                    #r2 = enclprobs[i].encl2
+                    #println("r2 is $(r2)")
+                    #strcol = replace(strcol, r1 => dlexucfg.delimiter)
+                    #strcol = replace(strcol, r2 => dlexucfg.delimiter)
+                    #println("strcol is now $(strcol)")
+                end    
+                strarray[idx] = strcol
+                #println("strarray[$(line)] IS NOW $(strarray[idx])")
+                idx = idx + 1           
+            end            
             line += 1
+            s = join(strarray)
+            println("s IS NOW $(s)")
         end
-        close(f)
+        close(lf)
     end
 
     return true
@@ -63,33 +90,6 @@ function check_qte_delm(s::String, srch::String)::String
     return s
 end
 
-#returns an array of strings with enclosures removed
 
-function delimited_array(str::String, encl1::String, encl2::String)::String
-
-    arr = []
-    occ = 1
-    while true
-        f = findnext(encl1, str, occ)
-        l = findnext(encl2, str, occ)
-        ds = ""
-        if !(f == nothing) && !(l == nothing)
-            ds = string(SubString(str, f[1] + 1, l[1] - 1))
-            occ = l[1] + 1
-            if length(ds) > 0
-                push!(arr, ds)
-            end
-        else
-            break
-        end
-    end
-
-    if length(arr) > 0
-       return arr[1]
-    else
-       return ""
-    end      
-
-end
 
 end # module 

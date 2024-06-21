469c8986538204256a3314f0299704de5286c320
Mark Nilan <mark7498@outlook.com>
2024-06-21 21:54:15 +0800

    dates cont 1

--- dlexu/src/tailordates.jl
+++ dlexu/src/tailordates.jl
@@ -9,7 +9,24 @@ function convert_dates(outv::Vector{String},dateprobs,dlexucfg)::Vector{String}
 
    dt = Date("2015-01-01",df)
 
-
+   for s in outv
+       strarray = split(s, dlexucfg.delimiter)
+       for column in strarray    
+          for i in eachindex(dateprobs) 
+              println("date template is $(dateprobs[i].date_template)")
+              df = DateFormat("$(dateprobs[i].date_template)");
+              try 
+                 dt = Date(column,df)
+                 println("dt is $(dt)")
+              catch
+                 continue 
+              end
+             
+              
+          end
+       end     
+   end   
+    
 
    for value in outv
        println(value)
